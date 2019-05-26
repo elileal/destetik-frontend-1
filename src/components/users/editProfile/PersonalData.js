495fc744e0bfa40c8833c54bab15feba622f4c6e
Pah Ferreira <pah.dsf@gmail.com>
2019-05-26 15:17:47 -0300

    implementing api user service

--- src/components/users/editProfile/PersonalData.js
+++ src/components/users/editProfile/PersonalData.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Form, FormGroup, Input, Button, Col, Row } from 'reactstrap';
+import { Form, FormGroup, Input, Button, Row } from 'reactstrap';
 import InputMask from 'react-input-mask';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import api from '../../../services/api';
+import Api from '../../../services/Api/index';
 
 import ConfirmModal from './ConfirmModal';
 
@@ -19,17 +19,10 @@ export class PersonalData extends Component {
   };
 
   async componentDidMount() {
-    const response = await api.get('/api/user/current');
-    const { name, email } = response.data;
-    let street, district, houseNumber;
-    street = district = houseNumber = '';
-    const phone = response.data.phone ? response.data.phone : '';
-    if (response.data.address) {
-      street = response.data.address.street;
-      district = response.data.address.district;
-      houseNumber = response.data.address.houseNumber;
-    }
-    this.setState({ name, email, phone, street, district, houseNumber });
+    const response = await Api.Users.current();
+    const { name, email } = response;
+    const phone = response.phone ? response.phone : '';
+    this.setState({ name, email, phone });
   }
 
   handleOnChange = e => {
@@ -38,23 +31,14 @@ export class PersonalData extends Component {
 
   handleSubmit = async e => {
     e.preventDefault();
-    const { name, email, street, district, houseNumber } = this.state;
-    const phone = this.state.phone
-      .split(' ')
-      .join('')
-      .split('-')
-      .join('');
-    const updatedUser = {
+    const { name, email } = this.state;
+    const phone = this.state.phone.split(' ').join('');
+    const userNewData = {
       name,
       email,
-      phone,
-      address: {
-        street,
-        district,
-        houseNumber
-      }
+      phone
     };
-    const response = await api.patch('/api/user/update', updatedUser);
+    const response = await Api.Users.update(userNewData);
     if (response.status === 200) {
       this.toggle();
     }
@@ -65,17 +49,24 @@ export class PersonalData extends Component {
   };
 
   render() {
-    const style = {
-      textAlign: 'left'
-    };
     const { visible } = this.state;
 
     return (
-      <div className="personal-data-container">
+      <>
         <ConfirmModal visible={visible} toggle={this.toggle} />
-        <Form style={style} method="POST" onSubmit={this.handleSubmit}>
+        <h4 style={{ textAlign: 'left' }}>Dados Pessoais</h4>
+        <Form
+          style={{
+            height: '90%',
+            textAlign: 'left',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between'
+          }}
+          method="POST"
+          onSubmit={this.handleSubmit}
+        >
           <FormGroup>
-            <h4>Dados Pessoais</h4>
             <FormGroup>
               Nome:
               <Input
@@ -101,48 +92,16 @@ export class PersonalData extends Component {
                 name="phone"
                 onChange={this.handleOnChange}
                 value={this.state.phone}
-                mask="+5\5 99 99999-9999"
+                mask="(83) 99999-9999"
                 alwaysShowMask={false}
               />
-            </FormGroup>
-          </FormGroup>
-          <FormGroup>
-            <h4>Localização</h4>
-            <FormGroup>
-              Endereço:
-              <Input
-                type="text"
-                name="street"
-                onChange={this.handleOnChange}
-                value={this.state.street}
-              />
-            </FormGroup>
-            <FormGroup row>
-              <Col>
-                Bairro:
-                <Input
-                  type="text"
-                  name="district"
-                  onChange={this.handleOnChange}
-                  value={this.state.district}
-                />
-              </Col>
-              <Col>
-                Número:
-                <Input
-                  type="text"
-                  name="houseNumber"
-                  onChange={this.handleOnChange}
-                  value={this.state.houseNumber}
-                />
-              </Col>
             </FormGroup>
           </FormGroup>
           <Row style={{ justifyContent: 'center', paddingTop: '4%' }}>
             <Button type="submit">Salvar</Button>
           </Row>
         </Form>
-      </div>
+      </>
     );
   }
 }
