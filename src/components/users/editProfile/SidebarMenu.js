bdd79386f26e868d5f9c6e8f6eddebc3ee1b93da
Pah Ferreira <pah.dsf@gmail.com>
2019-05-26 15:17:56 -0300

    Adding Location option

--- src/components/users/editProfile/SidebarMenu.js
+++ src/components/users/editProfile/SidebarMenu.js
@@ -9,24 +9,24 @@ class SidebarMenu extends React.Component {
     redirect: false,
     itemsMenu: [
       {
-        label: 'Editar Foto',
-        value: 'editar-foto',
-        icon: 'fa-camera'
-      },
-      {
         label: 'Dados Pessoais',
         value: 'dados-pessoais',
         icon: 'fa-user'
       },
       {
+        label: 'Localização',
+        value: 'localizacao',
+        icon: 'fa-map-marker-alt'
+      },
+      {
+        label: 'Editar Foto',
+        value: 'editar-foto',
+        icon: 'fa-camera'
+      },
+      {
         label: 'Adicionar Serviços',
         value: 'add-servicos',
         icon: 'fa-briefcase'
-      },
-      {
-        label: 'Histórico',
-        value: 'historico',
-        icon: 'fa-list-alt'
       },
       {
         label: 'Formas de Pagamento',
