d1ba7ba992c9ba603d1bbef132540628554a5558
Pah Ferreira <pah.dsf@gmail.com>
2019-05-26 15:18:27 -0300

    implementing api user service

--- src/services/Api/Users.js
+++ src/services/Api/Users.js
@@ -5,9 +5,41 @@ const getAll = async () => {
   return response.data;
 };
 
+const show = async userId => {
+  const response = await api.get(`/api/user/show/${userId}`);
+  return response.data;
+};
+
 const register = async userData =>
   await api.post('api/user/register', userData);
 
 const login = async userData => await api.post('api/user/login', userData);
 
-export default { getAll, register, login };
+const current = async () => {
+  const response = await api.get('/api/user/current');
+  return response.data;
+};
+
+const update = async userNewData => {
+  const response = await api.patch('/api/user/update', userNewData);
+  return response;
+};
+
+const updateImage = async (formData, config) => {
+  await api.patch('/api/user/image_update', formData, config);
+};
+
+const deleteAccount = async () => {
+  await api.delete('/api/user/delete');
+};
+
+export default {
+  getAll,
+  show,
+  register,
+  login,
+  current,
+  update,
+  updateImage,
+  deleteAccount
+};
